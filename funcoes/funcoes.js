a3a8fbfe15ebcd6a315d95f9f32975d38035f423
Rodolpho Erick <rodolphoerick@info.ufrn.br>
2020-04-17 23:04:41 -0300

    Alterações nas anotações de funções

--- funcoes/funcoes.js
+++ funcoes/funcoes.js
@@ -5,7 +5,7 @@ function getName() {
 }
 
 function logFn(fn) { //Recebe uma função por parâmetro.
-    console.log(fn()); //console.log aplicado no que a função retorna.
+    console.log(fn()); //console.log com uma função interna que fará ser exibido o que for passado a ela.
 }
 
 const logFnResult = logFn; //logFnResult passa a se comportar como a função logFn(), e na linha 13 é passado como argumento a função getName(). Esse é o conceito chave de uma função de primeira classe, o fato de poder atribuir ela para uma variável ou estrutura de dados.
