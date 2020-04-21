ab8da24bb0a1b71b3909c6b20afca5b37ceb7f21
Rodolpho Erick <rodolphoerick@info.ufrn.br>
2020-04-21 02:03:16 -0300

    Adições no exemplo de string

--- tipos/string/1-string.js
+++ tipos/string/1-string.js
@@ -10,7 +10,7 @@ console.log('\nArray com as posições separadas pelo delimitador: ', splittedTe
 const replacedText = 'Texto'.replace('Text', 'txeT');
 console.log('\nSubstituição de valor: ', replacedText);
 
-// Retorna a "fatia" de um valor
+// Retorna a "fatia" de um valor. Caso passe dois parâmetros, pega a letra a partir do primeiro parâmetro e exclui a letra correspondente ao último parâmetro passado.
 const lastChar = 'Texto'.slice(-1);
 console.log('\nÚltima letra de uma string: ', lastChar);
 
@@ -20,6 +20,6 @@ console.log('\nValor da string da primeira letra menos a última: ', allWithoutL
 const secondToEnd = 'Texto'.slice(1);
 console.log('\nValor da string da segunda letra até a última: ', secondToEnd);
 
-// Retorna N caracteres a partir de uma posição
+// Retorna N caracteres a partir de uma posição. Os parâmetros do substring é o começo e a quantidade de caracteres.
 const twoCharsBeforeFirtPosit = 'Texto'.substr(0, 2);
 console.log('\nAs duas primeiras letras são: ', twoCharsBeforeFirtPosit);
