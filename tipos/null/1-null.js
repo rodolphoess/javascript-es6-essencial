dff918be028a60ef4256532f2032c6af9c36834c
Rodolpho Erick <rodolphoerick@info.ufrn.br>
2020-04-21 14:08:35 -0300

    Exemplos de null e undefined

---
+++ tipos/null/1-null.js
@@ -0,0 +1,4 @@
+const nullVariable = null;
+
+console.log(nullVariable);
+console.log(typeof nullVariable); // O tipo do null por trás é um Object.
---
+++ tipos/undefined/1-undefined.js
@@ -0,0 +1,3 @@
+let undefinedVariable;
+
+console.log('Tipo da variável: ', typeof undefinedVariable); // Para o undefined, existe o tipo Undefined, diferentemente do null que não existe o tipo Null, no qual pertence ao tipo Object.
