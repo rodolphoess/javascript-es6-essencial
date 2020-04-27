e14c3a42b2760cd51082382b23e51a451cd9978e
Rodolpho Erick <rodolphoerick@info.ufrn.br>
2020-04-26 21:22:01 -0300

    Exemplos function

--- tipos/functions/1-functions.js
+++ tipos/functions/1-functions.js
@@ -20,7 +20,7 @@ console.log(fn());
 console.log('Retorno de fn() após adicionar uma propriedade na linha 18: ', fn.prop);
 
 // Funções podem receber parâmetros, que inclusive caso esses parâmetros repitam-se pode-se aplicar o conceito de Currying para não precisar ficar repetindo sempre os mesmos valores de parâmetros.
-const logValue = value => console.log(value);
-const logFnResult = fnParam => console.log(fnParam());
+const logValue = value => console.log(value); // O value nesse caso representa um parâmetro que é recebido na Arrow Function.
+const logFnResult = fnParam => console.log(fnParam()); // O fnParam nesse caso representa uma função que é recebida na Arrow Function.
 
 logFnResult(fn);
