3d981cada39697a938cf566ae92ad24c0e9a6bb5
Rodolpho Erick <rodolphoerick@info.ufrn.br>
2020-04-26 22:14:11 -0300

    Adição de comentários explicativos em functions

--- tipos/functions/2-functions.js
+++ tipos/functions/2-functions.js
@@ -1,17 +1,19 @@
 (() => {
     this.name = 'arrow function';
     const getNameArrowFn = () => this.name;
+    /* Na arrow function, na criação dela, o this da arrow function sempre vai referenciar o escopo como um todo desde a criação da arrow function. 
+    Ou seja, a referência será para o escopo da função da linha 1 a 17. */
 
     function getName() {
-        return this.name;
+        return this.name; // Já funções que não arrow functions vai sempre referenciar para o contexto em que ela foi executada.
     }
 
     const user = {
         name: 'Nome no objeto de execução',
-        getNameArrowFn,
-        getName
+        getNameArrowFn, // = getNameArrowFn: getNameArrowFn
+        getName // = getName: getName
     }
 
-    console.log(user.getNameArrowFn());
-    console.log(user.getName());
+    console.log(user.getNameArrowFn()); // Como trata-se de uma arrow function, então ela sempre vai apontar para o escopo da função.
+    console.log(user.getName()); // Como a função getName foi executada dentro de user, então ela referencia para o contexto interno de user, fazendo com que seja exibido o valor interno de user.name
 })();
